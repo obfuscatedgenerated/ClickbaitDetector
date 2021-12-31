84ec65adc80d29324b79d3fd63e07c2d48b9aa60
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2021-12-31 15:14:43 +0000

    working training code

--- main.py
+++ main.py
@@ -22,23 +22,31 @@ print("Dataset Credit: https://www.kaggle.com/amananandrai")
 
 def create_model(features):
     print("Constructing hub layer...")
-    print("This may take some time if you haven't run this before since the model needs to download.")
+    print(
+        "This may take some time if you haven't run this before since the model needs to download."
+    )
     model_url = "https://tfhub.dev/google/nnlm-en-dim50-with-normalization/2"
-    hub_layer = hub.KerasLayer(model_url, input_shape=[], dtype=tf.string, trainable=True)
+    hub_layer = hub.KerasLayer(
+        model_url, input_shape=[], dtype=tf.string, trainable=True
+    )
     print(hub_layer(features[:3]))
     print("Finishing...")
     model = tf.keras.Sequential()
-    #model = tf.keras.Sequential([layers.Dense(64), layers.Dense(1)])
+    # model = tf.keras.Sequential([layers.Dense(64), layers.Dense(1)])
     # body = tf.keras.Sequential([layers.Dense(64), layers.Dense(1)])
     # preprocessed_inputs = preprocessing_head(inputs)
     # result = body(preprocessed_inputs)
     # model = tf.keras.Model(inputs, result)
-    #model.add(tf.keras.Input(shape=(1,), dtype=tf.string))
-    model.add(tf.keras.layers.Dense(16, activation='relu'))
+    # model.add(tf.keras.Input(shape=(1,), dtype=tf.string))
+    model.add(hub_layer)
+    model.add(tf.keras.layers.Dense(16, activation="relu"))
     model.add(tf.keras.layers.Dense(1))
+    model.summary()
     print("Compiling...")
     model.compile(loss=tf.losses.MeanSquaredError(), optimizer=tf.optimizers.Adam())
+    print("Done!")
     return model
+
 
 def main():
     # Mount the training data
@@ -50,17 +58,17 @@ def main():
     print("Dimensions: ", train_data.ndim)
     train_data_features = train_data.copy().headline
     train_data_labels = train_data.copy().clickbait
-    train_data_features = np.array(train_data_features)
+    train_data_features = np.array(train_data_features,dtype=object)
+    train_data_labels = np.array(train_data_labels)
     print("Features:", train_data_features)
     print("Labels:", train_data_labels)
+    train_data_new = np.array([train_data_features, train_data_labels])
+    print("New:", train_data_new)
 
     # Create the model
     print("Creating model...")
     model = create_model(train_data_features)
 
-    # Infer shape
-    # model.fit(train_data_features, train_data_labels, epochs=1)
-    # model.summary()
 
     # Checkpoint path
     checkpoint_path = "clickbait_model/cp.ckpt"
@@ -72,8 +80,8 @@ def main():
     )
 
     # Evaluate the model
-    loss, acc = model.evaluate(train_data_features, train_data_labels, verbose=2)
-    print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
+    #loss, acc = model.evaluate(train_data_features, train_data_labels, verbose=2)
+    #print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
 
     # Loads the weights
     # model.load_weights(checkpoint_path)
@@ -83,14 +91,19 @@ def main():
     # print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
 
     # Train the model
+    x_val = train_data_features[:10000]
+    partial_x_train = train_data_features[10000:]
+    y_val = train_data_labels[:10000]
+    partial_y_train = train_data_labels[10000:]
     print("Training for 10 epochs...")
     model.fit(
-        x=train_data_features,
-        y=train_data_labels,
+        partial_x_train,
+        partial_y_train,
         batch_size=512,
         epochs=10,
         callbacks=[cp_callback],
-        verbose=1
+        validation_data=(x_val, y_val),
+        verbose=1,
     )
 
 
