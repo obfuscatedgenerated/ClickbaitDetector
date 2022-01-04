a40782ce58f8598bc4dcd0ba6de2e2bbd36fa046
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-01-04 23:54:42 +0000

    working convert command

--- convert_model.bat
+++ convert_model.bat
@@ -1,1 +1,1 @@
-tensorflowjs_converter --input_format=tf_saved_model --output_format=tfjs_graph_model --signature_name=serving_default --saved_model_tags=serve ./clickbait_model/model ./clickbait_model/js
+tensorflowjs_converter --input_format=keras_saved_model --metadata= ./clickbait_model/model/ ./clickbait_model/js/
