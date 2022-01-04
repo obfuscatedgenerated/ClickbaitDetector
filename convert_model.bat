50404375bcbe2e887fb19d28d8fb082856f70961
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-01-04 23:43:24 +0000

    convert script

---
+++ convert_model.bat
@@ -0,0 +1,1 @@
+tensorflowjs_converter --input_format=tf_saved_model --output_format=tfjs_graph_model --signature_name=serving_default --saved_model_tags=serve ./clickbait_model/model ./clickbait_model/js
